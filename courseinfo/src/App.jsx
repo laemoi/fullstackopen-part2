354429fe650f899ff18c752655527902980a31b8
Lassi Oinas <lassi.oinas@hotmail.com>
2024-04-01 14:59:56 +0300

    2.1: courseinfo step6

---
+++ courseinfo/src/App.jsx
@@ -0,0 +1,67 @@
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course.name}</h1>
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <div>
+      <p>{props.name} {props.exercises}</p>
+    </div>
+  )
+}
+
+const Content = ({parts}) => {
+  return (
+    <div>
+      {parts.map(part => <Part key={part.id} name={part.name} exercises={part.exercises} />)}
+    </div>
+  )
+}
+
+const Course = ({course}) => (
+  <div>
+    <Header course={course} />
+    <Content parts={course.parts} />
+  </div>
+)
+
+// TODO: modify to calculate total based on arbitrarily many courses
+const Total = (props) => {
+  return (
+    <div>
+      <p>Number of exercises {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App
