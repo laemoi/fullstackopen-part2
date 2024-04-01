240cdeb0377efabdd5f88c43e36946bc23773dc9
Lassi Oinas <lassi.oinas@hotmail.com>
2024-04-01 16:33:32 +0300

    2.6: phonebook step1

---
+++ phonebook/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const newPerson = { name: newName }
+    setPersons(persons.concat(newPerson))
+    setNewName("")
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(p => <div key={p.name}>{p.name}</div>)}
+    </div>
+  )
+}
+
+export default App
