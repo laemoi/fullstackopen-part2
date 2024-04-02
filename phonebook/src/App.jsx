bb4d6923c03df5cae37d23ad6a86af689df17890
Lassi Oinas <lassi.oinas@hotmail.com>
2024-04-02 11:46:15 +0300

    2.8: phonebook step3

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -5,19 +5,25 @@ const App = () => {
     { name: 'Arto Hellas' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState("")
 
   const handleSubmit = (event) => {
     event.preventDefault()
     if (persons.map(p => p.name).includes(newName)) {
       return alert(`${newName} is already added to phonebook`)
     }
-    const newPerson = { name: newName }
+    const newPerson = { name: newName, number: newNumber }
     setPersons(persons.concat(newPerson))
     setNewName("")
+    setNewNumber("")
   }
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -26,13 +32,15 @@ const App = () => {
       <form onSubmit={handleSubmit}>
         <div>
           name: <input value={newName} onChange={handleNameChange}/>
+          <br></br>
+          number: <input value={newNumber} onChange={handleNumberChange}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(p => <div key={p.name}>{p.name}</div>)}
+      {persons.map(p => <div key={p.name}>{p.name} {p.number}</div>)}
     </div>
   )
 }
