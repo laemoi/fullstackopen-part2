cfe745bdc383f0882481f82cefaee65ebc9f706d
Lassi Oinas <lassi.oinas@hotmail.com>
2024-04-02 14:34:49 +0300

    2.11: phonebook step6

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({filter, handleFilter}) =>
   <div>
@@ -24,16 +25,23 @@ const Persons = ({persons}) =>
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState("")
   const [filter, setFilter] = useState("")
-  const [shownPersons, setShownPersons] = useState(persons)
+  const [shownPersons, setShownPersons] = useState([])
+
+  useEffect( 
+    () => {
+      axios
+        .get("http://localhost:3001/persons")
+        .then(res => {
+          setPersons(res.data)
+          setShownPersons(res.data)
+        })
+    },  
+    []
+  )
 
   const handleSubmit = (event) => {
     event.preventDefault()
