00cbd6e7b55c5d889a3c315b192b87a1248ebe97
Lassi Oinas <lassi.oinas@hotmail.com>
2024-04-06 20:57:55 +0300

    2.18*: countries step1

---
+++ countries/src/App.jsx
@@ -0,0 +1,77 @@
+import { useState, useEffect } from 'react'
+import axios from "axios"
+
+const Country = ({n, capital, area, languages, flag}) => {
+  return (
+    <div>
+      <h1>{n}</h1>
+      <div>capital: {capital}</div>
+      <div>area: {area}</div>
+
+      <h3>Languages:</h3>
+      <ul>
+        {Object.values(languages).map((lang, index) => <li key={index}>{lang}</li>)}
+      </ul>
+      
+      <img src={flag} alt={`Flag of ${n}`}/>
+    </div>
+  )
+}
+
+const Display = ({matches}) => {
+  if (matches.length > 10) {
+    return <div>Too many matches, specify another filter</div>
+  }
+  else if (matches.length > 1) {
+    return matches.map(c => {
+      return <div key={c.name.official}>{c.name.common}</div>
+    })
+  }
+  else if (matches.length > 0) {
+    const c = matches[0]
+    return (
+      <Country
+        n={c.name.common}
+        capital={c.capital[0]}
+        area={c.area}
+        languages={c.languages}
+        flag={c.flags.png}
+      />
+    )
+  }
+  else {
+    <p>No countries matched the search.</p>
+  }
+}
+
+const App = () => {
+
+  const [search, setSearch] = useState("")
+  const [countries, setCountries] = useState([])
+  const [matchingCountries, setMatchingCountries] = useState([])
+
+  useEffect(() => {
+    axios
+      .get("https://studies.cs.helsinki.fi/restcountries/api/all")
+      .then(res => {
+        setCountries(res.data)
+        setMatchingCountries(res.data)
+      })
+  },
+  [])
+
+  const handleSearch = (event) => {
+    const s = event.target.value
+    setSearch(s)
+    setMatchingCountries(countries.filter(country => country.name.common.toLowerCase().includes(s)))
+  }
+
+  return (
+    <div>
+      Find countries: <input type="text" value={search} onChange={handleSearch}/>
+      <Display matches={matchingCountries} />
+    </div>
+  )
+}
+
+export default App
